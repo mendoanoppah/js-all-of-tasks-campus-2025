aa5f993aa2d93f913fdbd3f458b8bfe38a4327d5
mendoanoppah <ayassshy43@gmail.com>
2025-10-07 01:54:17 +0700

    rm subfolder .git/repo

---
+++ training/contoh-export.js
@@ -0,0 +1,11 @@
+// export default
+export default function greet(name, age) {
+  if (age <= 10) {
+    return `"my name is ${name}, and my age is ${age}"`
+  } else {
+    return `"MAMAMYOOOOO"`
+  }
+  return "hi"
+  return "mama"
+}
+
---
+++ training/contoh-import.js
@@ -0,0 +1,4 @@
+// contoh import variable *(export default)
+import greet from "./contoh-export.js"
+
+console.log(greet("ioo", String(11)))
